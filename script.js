cce241ffb649ffb88bc81468b2ca21a34733808a
Ftih07 <naufalfathi37@gmail.com>
2024-09-18 09:16:28 +0700

    first

--- script.js
+++ script.js
@@ -60,16 +60,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 // Aviation Safety
-        ScrollReveal({
-            reset: true,
-            distance: '60px',
-            duration: 2500,
-            delay: 400
-          });
-  
-          ScrollReveal().reveal('.main-title, .text-box, .hero-btn, .section-title', { delay: 500, origin: 'left' });
-          ScrollReveal().reveal('.sec-01 .image, .info', { delay: 600, origin: 'bottom' });
-          ScrollReveal().reveal('.text-box2', { delay: 500, origin: 'right' });
-          ScrollReveal().reveal('.media-icons i', { delay: 500, origin: 'bottom', interval: 200 });
-          ScrollReveal().reveal('.sec-02 .image, .sec-03 .image', { delay: 500, origin: 'top' });
-          ScrollReveal().reveal('.media-info li', { delay: 500, origin: 'left', interval: 200 });
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+  });
+
+  ScrollReveal().reveal('.main-title, .text-box, .hero-btn, .section-title', { delay: 500, origin: 'left' });
+  ScrollReveal().reveal('.sec-01 .image, .info', { delay: 600, origin: 'bottom' });
+  ScrollReveal().reveal('.text-box2', { delay: 500, origin: 'right' });
+  ScrollReveal().reveal('.media-icons i', { delay: 500, origin: 'bottom', interval: 200 });
+  ScrollReveal().reveal('.sec-02 .image, .sec-03 .image', { delay: 500, origin: 'top' });
+  ScrollReveal().reveal('.media-info li', { delay: 500, origin: 'left', interval: 200 });
